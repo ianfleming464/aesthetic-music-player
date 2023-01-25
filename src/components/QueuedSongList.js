f26c9adf6c8cebcf8aa950997dd6c6df65eee222
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-01-25 06:33:08 +0100

    Building out QueuedSongList component

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Avatar, IconButton, Typography } from '@mui/material';
+import { Delete } from '@mui/icons-material';
 
 function QueuedSongList() {
-  return <div>QueuedSongList</div>;
+  const song = {
+    title: 'LÜNE',
+    artist: 'MÖÖN',
+    thumbnail: 'http://img.youtube.com/vi/--ZtUFsIgMk/0.jpg',
+  };
+
+  return (
+    <div style={{ margin: '10px 0' }}>
+      <Typography color='textSecondary' variant='button'>
+        QUEUE (5)
+      </Typography>
+      {Array.from({ length: 5 }, () => song).map((song, i) => (
+        <QueuedSong key={i} song={song} />
+      ))}
+    </div>
+  );
+}
+
+function QueuedSong({ song }) {
+  const { thumbnail, artist, title } = song;
+  return (
+    <div>
+      <Avatar src={thumbnail} alt='Song thumbnail' />
+      <div>
+        <Typography variant='subtitle2'>{title}</Typography>
+        <Typography color='textSecondary' variant='body2'>
+          {artist}
+        </Typography>
+      </div>
+      <IconButton>
+        <Delete color='error' />
+      </IconButton>
+    </div>
+  );
 }
 
 export default QueuedSongList;
+
+// styled components
