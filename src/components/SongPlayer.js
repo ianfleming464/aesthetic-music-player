86ea3d185f69d363cf56c23c95124fbd3ce94cf9
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-02-07 22:13:52 +0100

    Added the ability to autoplay the next song in the queue. Also the next and previous buttons work.

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,6 +1,6 @@
 import { PlayArrow, SkipPrevious, SkipNext, Pause } from '@mui/icons-material';
 import { Card, CardContent, CardMedia, IconButton, Slider, Typography } from '@mui/material';
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
 import { appTheme } from '../theme';
 import QueuedSongList from './QueuedSongList';
 import { styled } from '@mui/material/styles';
@@ -16,6 +16,22 @@ function SongPlayer() {
   const [played, setPlayed] = useState(0);
   const [playedSeconds, setPlayedSeconds] = useState(0); // display time
   const [seeking, setSeeking] = useState(false); // controls when user is seeking through the song with the slider
+  const [positionInQueue, setPositionInQueue] = useState(0);
+
+  // The following hook is used to find out the position in the queue of whatever song has finished playing. If the current song playing's value (id) is NOT found (according to its id) in the queue, it returens a vaule of -1. The first song in the queue has index 0, second 1 and so on. This will help us figure out what to play next..
+  useEffect(() => {
+    const songIndex = data.queue.findIndex(song => song.id === state.song.id);
+    setPositionInQueue(songIndex);
+  }, [data.queue, state.song.id]);
+
+  // The next useEffect is used to automatically play the next song in the queue. Once we reach the end of the song in the player, it will jump to the next one.
+  useEffect(() => {
+    const nextSong = data.queue[positionInQueue + 1];
+    if (played >= 0.99 && nextSong) {
+      setPlayed(0);
+      dispatch({ type: 'SET_SONG', payload: { song: nextSong } });
+    }
+  }, [data.queue, played, dispatch, positionInQueue]);
 
   function handleTogglePlay() {
     dispatch(state.isPlaying ? { type: 'PAUSE_SONG' } : { type: 'PLAY_SONG' });
@@ -40,6 +56,20 @@ function SongPlayer() {
     return new Date(seconds * 1000).toISOString().substr(11, 8);
   }
 
+  function handlePlayPreviousSong() {
+    const previousSong = data.queue[positionInQueue - 1];
+    if (previousSong) {
+      dispatch({ type: 'SET_SONG', payload: { song: previousSong } });
+    }
+  }
+
+  function handlePlayNextSong() {
+    const nextSong = data.queue[positionInQueue + 1];
+    if (nextSong) {
+      dispatch({ type: 'SET_SONG', payload: { song: nextSong } });
+    }
+  }
+
   return (
     <>
       <PlayerContainer variant='outlined'>
@@ -53,13 +83,13 @@ function SongPlayer() {
             </Typography>
           </PlayerContent>
           <PlayerControls>
-            <IconButton>
+            <IconButton onClick={handlePlayPreviousSong}>
               <SkipPrevious />
             </IconButton>
             <IconButton onClick={handleTogglePlay}>
               {state.isPlaying ? <PlayerPauseIcon /> : <PlayerPlayIcon />}
             </IconButton>
-            <IconButton>
+            <IconButton onClick={handlePlayNextSong}>
               <SkipNext />
             </IconButton>
 
