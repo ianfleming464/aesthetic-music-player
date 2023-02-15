7ecf117bc21d8f275598a220739f9aaec95d168c
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-02-15 10:48:33 +0100

    Added link to portfolio, changed header and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import { HeadsetTwoTone } from '@mui/icons-material';
-import { AppBar, Toolbar, Typography, IconButton, Avatar } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import ComputerTwoToneIcon from '@mui/icons-material/ComputerTwoTone';
 
 function Header() {
   return (
     <AppBar position='fixed'>
-      <Toolbar>
+      <Toolbar sx={{ justifyContent: 'flex-start' }}>
         <HeadsetTwoTone />
         <HeaderTitle variant='h6' component='h1'>
           a e s t h e t i c - m u s i c - s h a r e
         </HeaderTitle>
-        {/* <IconButton sx={{ p: 0, display: 'flex-end' }}>
-          <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
-        </IconButton> */}
+        <Button
+          variant='contained'
+          href='https://ianflemingdeveloper.com/'
+          target='_blank'
+          endIcon={<ComputerTwoToneIcon />}
+          sx={{ marginLeft: 'auto' }}>
+          D e v e l o p e r - i n f o
+        </Button>
       </Toolbar>
     </AppBar>
   );
