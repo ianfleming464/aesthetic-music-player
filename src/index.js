855d15db0ea7766bac5b699c3ae8ae02d3197801
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-02-15 11:32:51 +0100

    Removed some redundant comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { appTheme, darkTheme } from './theme';
+import { appTheme } from './theme';
 import { ApolloProvider } from '@apollo/client';
 import client from './graphql/client';
 
@@ -11,7 +11,6 @@ root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <ThemeProvider theme={appTheme}>
-        {/* <ThemeProvider theme={darkTheme}> */}
         <CssBaseline />
         <App />
       </ThemeProvider>
